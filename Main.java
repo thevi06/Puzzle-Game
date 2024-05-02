0b04030e5b2b66ff1debb533dd9c39b94e395cca
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-05-02 09:31:55 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,2 +1,6 @@
+import java.util.List;
 public class Main {
-}
+    public static void main(String[] args) {
+
+        //call the method that get input form a file and store it as map
+        Map map = Parser.parse();
