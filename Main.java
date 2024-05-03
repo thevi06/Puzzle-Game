60dee2d520daa75302c7ff02f72a0674069827af
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-05-03 22:52:07 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,3 +4,6 @@ public class Main {
 
         //call the method that get input form a file and store it as map
         Map map = Parser.parse();
+
+        while (choice != 1 && choice != 2) {
+            // Ask the user to choose between loading from benchmark_series or text_file
