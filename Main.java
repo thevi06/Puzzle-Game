8d24b22e695827565712847fe57289ecfdb3b6dd
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2024-05-04 23:07:09 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,3 +7,6 @@ public class Main {
 
         while (choice != 1 && choice != 2) {
             // Ask the user to choose between loading from benchmark_series or text_file
+            System.out.println("Choose an option:");
+            System.out.println("1. Load from benchmark_series");
+            System.out.println("2. Load from text_file");
